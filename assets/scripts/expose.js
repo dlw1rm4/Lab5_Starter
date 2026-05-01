d40aa04c46284a93e318970e640fd4542882853a
dlw1rm4 <kaitlyn.loc101@gmail.com>
2026-05-01 13:03:54 -0700

    confetti

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,14 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const jsconfetti = new JSConfetti();
+
+  const hornSelect = document.getElementById('horn-select');
+  const playButton = document.querySelector('button');
+
+  playButton.addEventListener('click', () => {
+    if (hornSelect.value === 'party-horn') {
+      jsconfetti.addConfetti();
+    }
+  });
 }
