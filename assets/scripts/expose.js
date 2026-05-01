2705220c534f4bf8fbd174f2021300d6f2731fa5
dlw1rm4 <kaitlyn.loc101@gmail.com>
2026-05-01 13:32:06 -0700

    added confetti and audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -8,8 +8,30 @@ function init() {
 
   const hornSelect = document.getElementById('horn-select');
   const playButton = document.querySelector('button');
+  const audio = document.querySelector('audio');
+  const volumeSlider = document.getElementById('volume');
 
-  playButton.addEventListener('click', () => {
+  hornSelect.addEventListener('change', () => {
+    if (hornSelect.value === 'air-horn') {
+      audio.src = 'assets/audio/air-horn.mp3';
+    }
+    else if (hornSelect.value === 'car-horn') {
+      audio.src = 'assets/audio/car-horn.mp3';
+    }
+    else if (hornSelect.value === 'party-horn') {
+      audio.src = 'assets/audio/party-horn.mp3';
+    }
+  });
+
+  volumeSlider.addEventListener('input', () => {
+    audio.volume = volumeSlider.value / 100;
+  });
+
+    playButton.addEventListener('click', () => {
+    if (audio.src) {
+      audio.play();
+    }
+    
     if (hornSelect.value === 'party-horn') {
       jsconfetti.addConfetti();
     }
