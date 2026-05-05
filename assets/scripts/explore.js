45c47572903479bb53acac0953add9e2a8ff9174
Kaitlyn <kaitlyn.loc101@gmail.com>
2026-05-04 23:34:27 -0700

    fixed bugs

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,24 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const face_speak = document.querySelector('img');
+  const synth = window.speechSynthesis;
+
+  const talkButton = document.querySelector('button');
+  talkButton.addEventListener('click', () => {
+    const textToSpeak = "Welcome to Lab 5";
+    const utterance = new SpeechSynthesisUtterance(textToSpeak);
+
+    utterance.onstart = () => {
+      face_speak.src = 'assets/images/smiling-open.png'
+      face_speak.alt = "Smiling face with mouth open";
+    };
+
+    utterance.onend = () => {
+      face_speak.src = 'assets/images/smiling.png';
+      face_speak.alt = "Smiling face";
+    };
+
+    synth.speak(utterance);
+  });
 }
