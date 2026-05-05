8fba257c3c7e95e1cb9d4652e739292051d63a34
Kaitlyn <kaitlyn.loc101@gmail.com>
2026-05-04 23:58:58 -0700

    false cases for isStrongPassword

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,11 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+test('tests password: 1234abcd', () => {
+    expect(isStrongPassword('1234abcd')).toBe(false);
+});
+
+test('tests password: xyz', () => {
+    expect(isStrongPassword('xyz')).toBe(false);
+});
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,24 +4,56 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  const face_speak = document.querySelector('img');
-  const synth = window.speechSynthesis;
+    const voiceSelect = document.getElementById('voice-select');
+    const button = document.querySelector('button');
+    const textarea = document.querySelector('textarea');
+    const synth = window.speechSynthesis;
+    const face_speak = document.querySelector('img');
 
-  const talkButton = document.querySelector('button');
-  talkButton.addEventListener('click', () => {
-    const textToSpeak = "Welcome to Lab 5";
-    const utterance = new SpeechSynthesisUtterance(textToSpeak);
+    function populateVoiceList() {
+        if (typeof speechSynthesis === 'undefined') {
+            return;
+        }
+        const voices = synth.getVoices();
+        for (let i = 0; i < voices.length; i++) {
+            const option = document.createElement('option');
+            option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+            option.setAttribute('data-lang', voices[i].lang);
+            option.setAttribute('data-name', voices[i].name);
+            voiceSelect.appendChild(option);
+        }
+    }
 
-    utterance.onstart = () => {
-      face_speak.src = 'assets/images/smiling-open.png'
-      face_speak.alt = "Smiling face with mouth open";
-    };
+    if (synth.onvoiceschanged !== undefined) {
+        synth.onvoiceschanged = populateVoiceList;
+    }
+    populateVoiceList();
 
-    utterance.onend = () => {
-      face_speak.src = 'assets/images/smiling.png';
-      face_speak.alt = "Smiling face";
-    };
 
-    synth.speak(utterance);
-  });
+
+
+    button.addEventListener('click', () => {
+        const utterThis = new SpeechSynthesisUtterance(textarea.value);
+        const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+        for (let i = 0; i < synth.getVoices().length; i++) {
+            if (synth.getVoices()[i].name === selectedOption) {
+                utterThis.voice = synth.getVoices()[i];
+            }
+        }
+      
+        utterThis.onstart = () => {
+          face_speak.src = 'assets/images/smiling-open.png'
+          face_speak.alt = "Smiling face with mouth open";
+        };
+
+        utterThis.onend = () => {
+          face_speak.src = 'assets/images/smiling.png';
+          face_speak.alt = "Smiling face";
+        };
+        synth.speak(utterThis);
+    });
+        
+    
+      
+
 }
